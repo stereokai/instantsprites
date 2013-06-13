e6709504acc758f42227bcf34ea1bb22d53ae7c4
Stereokai <tom@stereokai.com>
2013-06-13 12:43:07 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-instantsprites
-==============
+Instant Sprites for Compass - By Stereokai
+===========================================
 
-Instant Sprites for Compass
+### What is this?
+
+This Node.js script generates a .scss file compatible and ready to compile with Compass,
+along with respective CSS classes to allow for easy management of CSS sprites.
+
+### What does it do?
+
+It reads all files in a directory, assuming they are all sprites with state suffixes, such as -normal, -hover and -down.
+
+It then generates the Compass code for magically generating a sprite image and a matching Compass sprite map, as well as SCSS classes that can help you overwrite background-position for each sprite-class and even each state.
+
+### How to use it?
+
+> node instantsprites.js <path/to/sprite/folder> from your console to generate the .scss file.
+And then @import it in your main scss file. Finally you have to include the sprite map in an input or class:
+
+	background-image: $<mapName>-sprites; // <mapName> is the name of the folder containing the sprites
+
+You're done!
+
+### How does it work?
+
+It is important to note, that this script assumes the folder it is given, is already configured as the images_dir
+in the Compass project config.rb. It also assumes that all files in this folder are png sprites.
+
+It then runs through all of the files looking for names which end with one of the states found in the `states` array.
+
+It will generate the code for Compass generates to generate a `$sprite-map` and a sprite asset, so you don't need to worry about that.
+
+### So how does it look like?
+
+The script automatically generates this mixin for you:
+
+	@mixin <mapName>-sprites-mixin($sprite-name, $normal: false [, more states], $overwriteX: false, $overwriteY: false)
+
+And it automatically generates CSS classes for each sprite type, for example:
+
+	&.sprite-with-states.normal {
+		@include <mapName>-sprites-mixin(sprite-with-states, $normal: true);
+	}
+	&.sprite-with-states.hover {
+		@include <mapName>-sprites-mixin(sprite-with-states, $hover: true);
+	}
+	&.sprite-with-states.down {
+		@include <mapName>-sprites-mixin(sprite-with-states, $down: true);
+	}
+	&.single-sprite {
+		@include <mapName>-sprites-mixin(single-sprite);
+	}
+
+### And what about overwriting `background-position`?
+
+Oh yes, that too. Once your code is generated, You can then manually edit the resulted SCSS to overwrite with your own position values:
+
+	&.sprite-with-states.down {
+		@include <mapName>-sprites-mixin(sprite-with-states, $down: true, $overwriteX: 6px, $overwriteY: 20px);
+	}
+
+### One last thing, what about single sprites with no states?
+
+We've got you covered! If your sprite file name doesn't end with a recognized state, you'll get this CSS:
+
+	&.single-sprite {
+		@include <mapName>-sprites-mixin(single-sprite);
+	}
---
+++ instantsprites.js
@@ -0,0 +1,125 @@
+// Instant Compass Sprites v1 - By Stereokai
+// ===========================================
+// See README for help
+
+	// add or remove states as you wish, an empty string means an else with no state after the last item in the array
+var states = ['normal', 'hover', 'down'],
+
+	fs = require('fs'),
+	filelist = fs.readdirSync(process.argv[2]),
+	slash = !process.argv[2].indexOf('/') ? '/' : '\\', // windows/unix?
+	path = process.argv[2].split(slash),
+	folder = path.pop() || path.pop(), // take care of a trailing slash
+	mapName = '$' + folder + '-sprites';
+	mixinName = folder + '-sprites-mixin';
+	currentSprite = [],
+	SCSS = '',
+	endsWith = function (str, suffix) {
+				// make sure we don't match against an empty string
+	    return 	suffix.length === 0 ? false : (str.indexOf(suffix, str.length - suffix.length) !== -1);
+	},
+	makeCSSClass = function (spriteName, statesString, state) {
+		statesString = (statesString ? ', ' + statesString : '');
+		return 	'.' + spriteName +
+				(state ? '.' + state : '') +
+				' {\n	@include ' + mixinName + '(' + spriteName + statesString + ');\n}\n';
+	},
+	processSprite = function () {
+		var spriteStates = {},
+			matchedSpriteName = '',
+			matches = 0;
+		// initiate flags for all states
+		for (i = 0; i < states.length; i++) {
+			spriteStates[states[i]] = false;	
+		}
+
+		while (currentSprite.length > 0) {
+			var state = currentSprite.shift(),
+				match = false;
+			// if we covered all states for this class let's skip this part
+			if (matches < states.length) {
+				for (i = 0; i < states.length; i++) {
+					if (endsWith(state, states[i])) {
+						spriteStates[states[i]] = true;
+						match = true;
+						matches++;
+
+						// store sprite name
+						matchedSpriteName = state.substr(0, state.length - states[i].length - 1);
+						break; 
+					}
+				}
+				// if no match, this is not a state we recognize,
+				// so this is in fact a sprite on its own, only with a similar name
+				if (!match) {
+					SCSS += makeCSSClass(state);
+				}
+			// from this point on these are all sprites with no recognized states
+			} else {
+				SCSS += makeCSSClass(state);
+			}
+		}
+
+		if (matches > 0) {
+			for (i = 0; i < states.length; i++) {
+				if (spriteStates[states[i]]) {
+					// build parameters for states
+					SCSS += makeCSSClass(matchedSpriteName, '$' + states[i] + ': true', states[i]);
+				}
+			}
+		}
+
+		// reset stack
+		currentSprite = [];
+	},
+	makeStateIf = function (state) {
+		return	'		@if $' + state + ' {\n' +
+				'			$pos: sprite-position(' + mapName + ', $sprite-name + "-' + state + '", 50%, 5%);\n		}\n';
+	};
+
+SCSS += mapName + ': sprite-map("' + folder + '/*.png", $position: 50%, $spacing: 20px);\n';
+SCSS += '@mixin ' + mixinName + ' (\n	$sprite-name,\n';
+var mixinswrap = '	@if ($' + states.join(' or $') + ') {\n';
+for (i = 0, params = mixins = ''; i < states.length; i++) {
+	params += (states[i] !== '' ? '	$' + states[i] + ': false,\n' : '');
+	mixins += makeStateIf(states[i]);
+	if (i == states.length - 1) { SCSS += params + '	$overwriteX: false,\n	$overwriteY: false' + ') {\n	$pos: "";\n\n' + mixinswrap + mixins; }
+}
+SCSS +=	'	}\n	@else {\n' +
+		'		$pos: sprite-position(' + mapName + ', $sprite-name, 50%, 5%);\n	}\n\n' +
+		'	$x: if($overwriteX, $overwriteX, nth($pos, 1));\n' +
+		'	$y: if($overwriteY, $overwriteY, nth($pos, 2));\n' +
+		'	background-position: $x $y;\n' +
+		'}\n\n';
+
+while (filelist.length > -1) {
+	if (filelist.length === 0 && currentSprite.length > 0) {
+		// this is the last sprite
+		processSprite();
+		break;
+	}
+
+	var fileNoExt = filelist.shift();
+		fileNoExt = fileNoExt.substr(0, fileNoExt.lastIndexOf('.'));
+
+	if (currentSprite.length == 0) {
+		// starting a new sprite
+		currentSprite.push(fileNoExt);
+	} else {
+		// check if we're still on the same sprite (check for identical file names, bar the state suffix)
+		var spr1 = fileNoExt.substring(0, fileNoExt.lastIndexOf('-')),
+			spr2 = currentSprite[0].substring(0, currentSprite[0].lastIndexOf('-'));
+		if (spr1.indexOf(spr2) === 0 || spr2.indexOf(spr1) === 0) {
+			// adding new state to current sprite
+			currentSprite.push(fileNoExt);
+		} else {
+			// we finished running over this sprite's different states
+			// now let's process it and start traversing the next sprite
+			processSprite();
+			currentSprite.push(fileNoExt);
+		}
+	}
+
+}
+
+fs.writeFile(process.argv[2] + slash + '_' + folder + '-sprites.scss', SCSS);
